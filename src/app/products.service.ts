4ad861920434770ba8cef5d3536d8813e879d93a
Andres <cheche3064@gmail.com>
2020-10-29 11:00:34 -0600

    Rutas

---
+++ src/app/products.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { Product} from './product.model';
+import { ProductsComponent } from './products/products.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/thanos.jpg',
+      title: 'Figura de Thanos',
+      price: 120,
+      description: 'Avengers'
+    },
+    {
+      id: '2',
+      image: 'assets/images/ash.jpg',
+      title: 'Figura de Ash',
+      price: 120,
+      description: 'Pokemon'
+    },
+    {
+      id: '3',
+      image: 'assets/images/carreras_control.jpg',
+      title: 'Carrito amarillo',
+      price: 600,
+      description: 'Carrito de carreras a control remoto'
+    },
+    {
+      id: '4',
+      image: 'assets/images/freezer.jpg',
+      title: 'Freezer',
+      price: 120,
+      description: 'Dragon ball Z'
+    },
+    {
+      id: '5',
+      image: 'assets/images/sony.jpg',
+      title: 'Sony',
+      price: 120,
+      description: 'Figura de Sony'
+    },
+    {
+      id: '6',
+      image: 'assets/images/ferrari.jpg',
+      title: 'Stickers',
+      price: 120,
+      description: 'Carrito ferrari'
+    }
+  ];
+
+  constructor() { }
+
+  getAllProducts() {
+    return this.products;
+  }
+
+  getProduct(id:string){
+    return this.products.find(item => id===item.id);
+  }
+}
